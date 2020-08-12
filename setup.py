da4a0717d3f368cd57292f187b25b4c9bdcae511
romaincaraes <hello@romaincaraes.fr>
2020-08-12 13:43:15 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+from setuptools import setup, find_packages
+
+import airtable
+
+setup(
+    name="airtable",
+    version=airtable.__version__,
+    license=airtable.__license__,
+    author=airtable.__author__,
+    author_email="hello@romaincaraes.fr",
+    description="Python wrapper for Airtable API",
+    long_description=open("README.md").read(),
+    url="https://github.com/romaincaraes/airtable-python/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 1 - Planning",
+        "Natural Language :: French",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Database",
+    ],
+)
